601e619ae51b5ebf9aa058060c8faed8ca863e74
Ju-lotty <jjg991201@gmail.com>
2022-09-06 17:57:59 +0900

    move Activity to HomeFragment

--- app/src/main/java/com/project/flowergarden/MainActivityUser.kt
+++ app/src/main/java/com/project/flowergarden/MainActivityUser.kt
@@ -2,74 +2,53 @@ package com.project.flowergarden
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.recyclerview.widget.LinearLayoutManager
+
+import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
-import com.google.firebase.database.ktx.database
 import com.project.flowergarden.databinding.ActivityMainUserBinding
-import com.project.flowergarden.entity.OwnerEntity
 import com.project.flowergarden.entity.StoreAdapter
+import com.project.flowergarden.userfragment.Home
+import com.project.flowergarden.userfragment.Information
+import com.project.flowergarden.userfragment.Like
+import com.project.flowergarden.userfragment.NearLocation
 
 class MainActivityUser : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainUserBinding
 
-    private var auth: FirebaseAuth? = null //파이어베이스 인증
-    private val adapter = StoreAdapter()
-
-    //유저 정보 불러오기 (아이디, 닉네임 등)
-    private var user: FirebaseUser? = null
-    private lateinit var UserDB: DatabaseReference //실시간 데이터베이스
-    private lateinit var OwnerDB: DatabaseReference
-    private var userID: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        viewFlipper()
-        binding.storeList.layoutManager = LinearLayoutManager(this)
-        binding.storeList.adapter = adapter
-        auth = FirebaseAuth.getInstance()
 
-        user = FirebaseAuth.getInstance().currentUser
-        OwnerDB = FirebaseDatabase.getInstance().getReference("Owner")
-        UserDB = FirebaseDatabase.getInstance().getReference("User")
-        userID = user!!.uid
-        OwnerDB.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                adapter.storeList.add(OwnerEntity(snapshot.value.toString(),"",""))
-                adapter.notifyDataSetChanged()
-                Log.e("User", "")
+        val home1 = Home()
+        val home2 = NearLocation()
+        val home3 = Like()
+        val home4 = Information()
+        showFragment(home1)
 
+        binding.bottomNavigationView.run{
+            setOnItemSelectedListener { it ->
+                when(it.itemId){
+                    R.id.ic_home -> showFragment(home1)
+                    R.id.ic_map -> showFragment(home2)
+                    R.id.ic_like-> showFragment(home3)
+                    R.id.ic_information -> showFragment(home4)
+                    else -> true
+                }
             }
-
-            override fun onCancelled(error: DatabaseError) {
-
-            }
-
-        })
-        UserDB.child(userID!!).addListenerForSingleValueEvent(object : ValueEventListener {
-
-            override fun onDataChange(snapshot: DataSnapshot) {
-                Log.e("2", "${snapshot.value}")
-                val nickname = snapshot.child("nickname").value.toString()
-                binding.userNameTextView.text = "${nickname.toString()}님 환영합니다!"
-                //val nickname = snapshot.value
-                //binding.userNameTextView.text = "${nickname.toString()}"
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-            }
-        })
+        }
     }
 
-    private fun viewFlipper() = with(binding) {
-        viewFlipper.startFlipping()
-        viewFlipper.flipInterval = 3000
-        viewFlipper.setInAnimation(this@MainActivityUser, android.R.anim.slide_in_left)
-        viewFlipper.setOutAnimation(this@MainActivityUser, android.R.anim.slide_out_right)
+
+    private fun showFragment(fragment: Fragment): Boolean{
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.container,fragment)
+            commit()
+        }
+        return true
     }
+
 }
---
+++ app/src/main/java/com/project/flowergarden/userfragment/Home.kt
@@ -0,0 +1,106 @@
+package com.project.flowergarden.userfragment
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import com.project.flowergarden.R
+import com.project.flowergarden.databinding.FragmentHomeBinding
+import com.project.flowergarden.entity.OwnerEntity
+import com.project.flowergarden.entity.StoreAdapter
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Home.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Home : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    private var auth: FirebaseAuth? = null //파이어베이스 인증
+    private val adapter = StoreAdapter()
+
+    //유저 정보 불러오기 (아이디, 닉네임 등)
+    private var user: FirebaseUser? = null
+    private lateinit var UserDB: DatabaseReference //실시간 데이터베이스
+    private lateinit var OwnerDB: DatabaseReference
+    private var userID: String? = null
+
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewFlipper()
+        binding.storeList.layoutManager = LinearLayoutManager(activity)
+        binding.storeList.adapter = adapter
+        auth = FirebaseAuth.getInstance()
+
+        user = FirebaseAuth.getInstance().currentUser
+        OwnerDB = FirebaseDatabase.getInstance().getReference("Owner")
+        UserDB = FirebaseDatabase.getInstance().getReference("User")
+        userID = user!!.uid
+
+        OwnerDB.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                adapter.storeList.add(OwnerEntity(snapshot.value.toString(), "", ""))
+                adapter.notifyDataSetChanged()
+                Log.e("User", "")
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+        UserDB.child(userID!!).addListenerForSingleValueEvent(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Log.e("2", "${snapshot.value}")
+                val nickname = snapshot.child("nickname").value.toString()
+                binding.userNameTextView.text = "${nickname.toString()}님 환영합니다!"
+                //val nickname = snapshot.value
+                //binding.userNameTextView.text = "${nickname.toString()}"
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+    }
+
+    private fun viewFlipper() = with(binding) {
+        viewFlipper.startFlipping()
+        viewFlipper.flipInterval = 3000
+        viewFlipper.setInAnimation(activity?.applicationContext, android.R.anim.slide_in_left)
+        viewFlipper.setOutAnimation(activity?.applicationContext, android.R.anim.slide_out_right)
+    }
+}

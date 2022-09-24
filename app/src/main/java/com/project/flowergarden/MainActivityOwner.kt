03bd1a241a021338eedf695e73818e87bdeb6416
Ju-lotty <jjg991201@gmail.com>
2022-09-24 17:31:51 +0900

    Owner Main Photo

--- app/src/main/java/com/project/flowergarden/MainActivityOwner.kt
+++ app/src/main/java/com/project/flowergarden/MainActivityOwner.kt
@@ -1,11 +1,21 @@
 package com.project.flowergarden
 
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 import com.project.flowergarden.databinding.ActivityMainOwnerBinding
+import kotlinx.android.synthetic.main.activity_main_owner.*
 
 
 class MainActivityOwner : AppCompatActivity() {
@@ -23,8 +33,8 @@ class MainActivityOwner : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainOwnerBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        auth = FirebaseAuth.getInstance()
 
+        auth = FirebaseAuth.getInstance()
         user = FirebaseAuth.getInstance().currentUser
         OwnerDB = FirebaseDatabase.getInstance().getReference("Owner")
         userID = user!!.uid
@@ -33,16 +43,64 @@ class MainActivityOwner : AppCompatActivity() {
 
             override fun onDataChange(snapshot: DataSnapshot) {
                 val nickname = snapshot.child("storename").value.toString()
-                binding.nickNameTextView.text = "${nickname}"
+                binding.nickNameTextView.text = nickname
                 val address = snapshot.child("address").value.toString()
-                binding.addressTextView.text = "${address}"
-                //val nickname = snapshot.value
-                //binding.nickNameTextView.text = "${nickname.toString()}"
+                binding.addressTextView.text = address
             }
-
             override fun onCancelled(error: DatabaseError) {
                 TODO("Not yet implemented")
             }
         })
+        initAddImageButton()
+    }
+
+    private fun initAddImageButton() {
+        storeImageButton.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                -> { // 갤러리로 이동
+                    getImageFromAlbum()
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                    //거부 했을 때 (생략)
+                }
+                else -> {
+                    // 처음 권한을 시도했을 때 띄움
+                    requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),REQUEST_FIRST)
+                }
+            }
+        }
+    }
+
+    private fun getImageFromAlbum() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        //모든 type
+        intent.type = "image/*"
+        startActivityForResult(intent,REQUEST_GET_IMAGE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode != Activity.RESULT_OK) {
+            Toast.makeText(this,"잘못된 접근입니다",Toast.LENGTH_SHORT).show()
+            return
+        }
+        when(requestCode){
+            REQUEST_GET_IMAGE -> {
+                val selectedImageURI : Uri? = data?.data
+                if( selectedImageURI != null ) {
+                    val imageView = findViewById<ImageView>(R.id.storeImageButton)
+                    imageView.setImageURI(selectedImageURI)
+                }
+                else {
+                    Toast.makeText(this,"Error",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val REQUEST_FIRST = 1000
+        const val REQUEST_GET_IMAGE = 2000
     }
 }
